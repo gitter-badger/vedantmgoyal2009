902c3109e7e624c42f7bfbe8f6b7676b1cedd259
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-07-22 23:34:43 +0530

    Update myScript.ps1

--- myScript.ps1
+++ myScript.ps1
@@ -2,26 +2,21 @@ $workingDir = Get-Location
 $ProgressPreference = 'SilentlyContinue'
 $fileDownload = [System.Net.WebClient]::new()
 Import-Module -Name Appx -UseWindowsPowershell
-
 $fileDownload.DownloadFile("https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx","$workingDir\Microsoft.VCLibs.x64.14.00.Desktop.appx") 
 Add-AppxPackage -Path "$workingDir\Microsoft.VCLibs.x64.14.00.Desktop.appx"
 Write-Host "Successfully installed Microsoft.VCLibs.x64.14.00.Desktop.appx"
-
 $fileDownload.DownloadFile("https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle","$workingDir\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
 Add-AppxPackage -Path "$workingDir\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 Write-Host "Successfully installed Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-
 ### Source: https://github.com/microsoft/winget-cli/issues/1321 
-Install-Module NtObjectManager -Force  # Install NtObjectManager module
-
-# Create reparse point 
+    # Install NtObjectManager module
+Install-Module NtObjectManager -Force
+    # Create reparse point 
 $installationPath = (Get-AppxPackage Microsoft.DesktopAppInstaller).InstallLocation
 Set-ExecutionAlias -Path "C:\Windows\System32\winget.exe" -PackageName "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe" -EntryPoint "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget" -Target "$installationPath\AppInstallerCLI.exe" -AppType Desktop -Version 3
 explorer.exe "shell:appsFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget"
 ### End
-
 git clone https://github.com/microsoft/winget-pkgs.git
-
 if (-Not (Get-Command "winget" -ErrorAction "SilentlyContinue")) {
     Write-Host "WinGet is not installed yet." -ForegroundColor Red
     return
@@ -42,7 +37,11 @@ if (-Not (Get-Command "winget" -ErrorAction "SilentlyContinue")) {
             }
             Write-Host "$responseCode`: $trimUrl"
             "$responseCode`: $trimUrl" | Out-File -Append -FilePath $workingDir\log.txt
-            Remove-Item $workingDir\$filename
+            try {
+                Remove-Item $workingDir\$filename
+            } catch {
+                Write-Host "File cannot be downloaded."
+            }
         }
     }
     Write-Host "Completed!"
