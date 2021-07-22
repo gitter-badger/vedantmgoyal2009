1e3cbe9b193e27181746dbf1e72886396f89e13b
Vedant Mohan Goyal <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-07-22 21:30:58 +0530

    Create myScript.ps1

---
+++ myScript.ps1
@@ -0,0 +1,48 @@
+$workingDir = Get-Location
+$ProgressPreference = 'SilentlyContinue'
+$fileDownload = [System.Net.WebClient]::new()
+
+$fileDownload.DownloadFile("https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx","$workingDir\Microsoft.VCLibs.x64.14.00.Desktop.appx") 
+Add-AppxPackage -Path "$workingDir\Microsoft.VCLibs.x64.14.00.Desktop.appx"
+Write-Host "Successfully installed Microsoft.VCLibs.x64.14.00.Desktop.appx"
+
+$fileDownload.DownloadFile("https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle","$workingDir\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
+Add-AppxPackage -Path "$workingDir\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+Write-Host "Successfully installed Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+
+### Source: https://github.com/microsoft/winget-cli/issues/1321 
+Install-Module NtObjectManager -Force  # Install NtObjectManager module
+
+# Create reparse point 
+$installationPath = (Get-AppxPackage Microsoft.DesktopAppInstaller).InstallLocation
+Set-ExecutionAlias -Path "C:\Windows\System32\winget.exe" -PackageName "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe" -EntryPoint "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget" -Target "$installationPath\AppInstallerCLI.exe" -AppType Desktop -Version 3
+explorer.exe "shell:appsFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget"
+### End
+
+git clone https://github.com/microsoft/winget-pkgs.git
+
+if (-Not (Get-Command "winget" -ErrorAction "SilentlyContinue")) {
+    Write-Host "WinGet is not installed yet." -ForegroundColor Red
+    return
+} else {
+    Write-Host "`nChecking HTTP Response codes for Download URLs...`n"
+    $manifestsDir = "$workingDir\winget-pkgs\manifests"
+    Foreach($i in (Get-ChildItem -Path $manifestsDir -Directory -Recurse -Depth 6 | Select FullName)) {
+        if(((Get-ChildItem -Directory -Path $i.FullName).Count) -eq 0) {
+            $manifestPath = $i.FullName
+            $DownUrl = (winget show --manifest "$manifestPath" | Select-String -Pattern "Download Url").ToString()
+            $trimUrl = $DownUrl.TrimStart("Download Url: ")
+            $filename = ($trimUrl -split '\/')[-1]
+            try {
+                $responseTest = (Invoke-WebRequest -SkipHttpErrorCheck -Uri $trimUrl -Method Get -OutFile "$workingDir\$filename" -PassThru) | Select StatusCode
+                $responseCode = $responseTest.StatusCode
+            } catch {
+                $responseCode = $_.Exception.responseTest.StatusCode.value__
+            }
+            Write-Host "$responseCode`: $trimUrl"
+            "$responseCode`: $trimUrl" | Out-File -Append -FilePath $workingDir\log.txt
+            Remove-Item $workingDir\$filename
+        }
+    }
+    Write-Host "Completed!"
+}
