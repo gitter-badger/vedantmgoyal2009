f57cbdf94dd5bbbc851900c0d1b30778e74ce5a5
Bittu <83997633+vedantmgoyal2009@users.noreply.github.com>
2021-08-06 12:33:13 +0530

    Update SumDigitsNoWith7.java

--- BlueJ_10/Arrays/SumDigitsNoWith7.java
+++ BlueJ_10/Arrays/SumDigitsNoWith7.java
@@ -4,17 +4,14 @@ public class SumDigitsNoWith7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of blocks of array: ");
-        int n=sc.nextInt(),arr[] = new int[n],s=0;
-        boolean contains7 = false;
-        for(int i=0;i<n;i++,contains7=false) {
+        int n=sc.nextInt(),arr[] = new int[n],s=0,j=0;
+        for(int i=0;i<n;i++) {
             System.out.println("Enter value for "+i+" block of array: ");
             arr[i]=sc.nextInt();
-            for(int j=arr[i];j!=0;j=j/10)
-                if(j%10==7) {
-                    contains7=true;
+            for(j=arr[i];j!=0;j=j/10)
+                if(j%10==7)
                     break;
-                }
-            if(contains7)
+            if(j!=0)
 	            for(int k=arr[i];k!=0;k=k/10)
 	                s+=k%10;
         }
